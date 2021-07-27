d4dc84ba74951a0054a67a4fc985f5fbd3dff65a
Michael Murray <v823061@global.ual.com>
2021-07-27 18:00:45 -0500

    Initial commit

---
+++ RadPasswordGenerator/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace RadPasswordGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.txtNumOfChars = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(84, 138);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(643, 22);
+            this.txtPassword.TabIndex = 2;
+            this.txtPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(316, 222);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(184, 41);
+            this.btnGenerate.TabIndex = 3;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // txtNumOfChars
+            // 
+            this.txtNumOfChars.Location = new System.Drawing.Point(316, 81);
+            this.txtNumOfChars.Name = "txtNumOfChars";
+            this.txtNumOfChars.Size = new System.Drawing.Size(78, 22);
+            this.txtNumOfChars.TabIndex = 1;
+            this.txtNumOfChars.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNumOfChars_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(226, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Number of characters to generate.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(138, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(552, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "MJMTECHSERVICES PASSWORD GENERATOR";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(796, 286);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNumOfChars);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.txtPassword);
+            this.Name = "Form1";
+            this.Text = "Password Generator v1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.TextBox txtNumOfChars;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ RadPasswordGenerator/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RadPasswordGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            int len = Convert.ToInt32(txtNumOfChars.Text);
+
+            StringBuilder sbPass = new StringBuilder();
+
+            for (int i = 0; i < len; i++)
+            {
+                sbPass.Append(SeedRandom());
+            }
+
+            txtPassword.Text = sbPass.ToString();
+        }
+
+        private string SeedRandom()
+        {
+            int asciiLow = 33;
+            int asciiHigh = 126;
+
+            int guidhash = (Guid.NewGuid().GetHashCode());
+
+            DateTime dt = DateTime.Now;
+
+            string dtticks = dt.Ticks.ToString();
+
+            int smallticksLength = dtticks.Length;
+
+            char[] ticksArray = new char[smallticksLength];
+
+            ticksArray = dtticks.ToCharArray();
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < smallticksLength; i++)
+            {
+
+                if (i >= (smallticksLength / 2))
+                    sb.Append(ticksArray[i].ToString());
+
+            }
+
+
+            int smallticks = Convert.ToInt32(sb.ToString());
+            int seed =  guidhash + smallticks;
+
+            Random randObj = new Random(seed);
+
+            int randout = randObj.Next(asciiLow, asciiHigh);
+            char c = Convert.ToChar(randout);
+
+            string passChar = c.ToString();
+            return passChar;
+
+        }
+
+        private void txtNumOfChars_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
